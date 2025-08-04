3f31b57fefe8e94a45b73a42759586c36c95d7d1
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2025-08-04 20:36:11 -0300

    refactor: integrate ProcessorHealthMonitor for improved queue consumption logic

--- pagamentos/src/main/java/br/com/rinha/pagamentos/consumer/RetryQueueConsumer.java
+++ pagamentos/src/main/java/br/com/rinha/pagamentos/consumer/RetryQueueConsumer.java
@@ -1,5 +1,6 @@
 package br.com.rinha.pagamentos.consumer;
 
+import br.com.rinha.pagamentos.health.ProcessorHealthMonitor;
 import br.com.rinha.pagamentos.model.QueuedPayment;
 import br.com.rinha.pagamentos.service.PaymentService;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -20,18 +21,22 @@ public class RetryQueueConsumer implements ApplicationListener<ApplicationReadyE
 	private static final String PROCESSING_QUEUE_KEY = "payments:processing-queue";
 	private static final int BATCH_SIZE = 70;
 	private static final Duration EMPTY_QUEUE_DELAY = Duration.ofMillis(200);
+	private static final Duration PROCESSORS_UNAVAILABLE_DELAY = Duration.ofMillis(100);
 
 	private final ReactiveRedisTemplate<String, QueuedPayment> reactiveRedisTemplate;
 	private final PaymentService paymentService;
+	private final ProcessorHealthMonitor processorHealthMonitor;
 
 	@Value("${retry.consumer.concurrency}")
 	private int concurrencyLevel;
 
 	public RetryQueueConsumer(
 			@Qualifier("reactiveQueuedRedisTemplate") ReactiveRedisTemplate<String, QueuedPayment> reactiveRedisTemplate,
-			PaymentService paymentService) {
+			PaymentService paymentService,
+			ProcessorHealthMonitor processorHealthMonitor) {
 		this.reactiveRedisTemplate = reactiveRedisTemplate;
 		this.paymentService = paymentService;
+		this.processorHealthMonitor = processorHealthMonitor;
 	}
 
 	@Override
@@ -42,9 +47,17 @@ public class RetryQueueConsumer implements ApplicationListener<ApplicationReadyE
 	}
 
 	private Mono<Void> consumeFromQueue() {
-		return reactiveRedisTemplate.opsForList()
-				.rightPop(PROCESSING_QUEUE_KEY, BATCH_SIZE)
-				.switchIfEmpty(Flux.defer(() -> Flux.<QueuedPayment>empty().delaySubscription(EMPTY_QUEUE_DELAY)))
+		return Flux.defer(() -> {
+					boolean canProcess = processorHealthMonitor.isDefaultProcessorAvailable() || processorHealthMonitor.isFallbackProcessorAvailable();
+
+					if (canProcess) {
+						return reactiveRedisTemplate.opsForList()
+								.rightPop(PROCESSING_QUEUE_KEY, BATCH_SIZE)
+								.switchIfEmpty(Flux.defer(() -> Flux.<QueuedPayment>empty().delaySubscription(EMPTY_QUEUE_DELAY)));
+					} else {
+						return Flux.<QueuedPayment>empty().delaySubscription(PROCESSORS_UNAVAILABLE_DELAY);
+					}
+				})
 				.repeat()
 				.parallel(concurrencyLevel)
 				.runOn(Schedulers.parallel())
